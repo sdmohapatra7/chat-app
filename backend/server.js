99faf53bc660c6988f76dedfbbe5b93676a686d1
Shaktidhar <sdmohapatra7@gmail.com>
2024-03-10 12:47:38 +0530

    put the frontend connection help in times of hosting

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import path from "path";
 import cookieParser from "cookie-parser";
 import express from 'express'
 import authRoutes from './routes/authRoutes.js'
@@ -7,6 +8,7 @@ import messageRoutes from './routes/messageRoutes.js'
 import {db} from './config/mongoose.js';
 const app = express();
 
+const __dirname = path.resolve();
 dotenv.config();
 const PORT = process.env.PORT;
 db();
@@ -17,5 +19,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use('/api/users',userRoutes);
 app.use("/api/messages", messageRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT,()=> console.log(`Server Running on Port ${PORT}`));
