751d1b00887b122f133c4d32fe301bc28500b7fd
Shaktidhar <sdmohapatra7@gmail.com>
2024-03-10 12:17:06 +0530

    provide auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import dotenv from 'dotenv';
-
+import cookieParser from "cookie-parser";
 import express from 'express'
-import userRouter from './routes/userRoute.js'
+import authRoutes from './routes/authRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {db} from './config/mongoose.js';
 const app = express();
 
 dotenv.config();
+const PORT = process.env.PORT;
 db();
 
-const PORT = process.env.PORT;
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-
-app.use('/api/auth',userRouter)
+app.use("/api/auth", authRoutes);
+app.use('/api/auth',userRoutes)
 
 app.listen(PORT,()=> console.log(`Server Running on Port ${PORT}`));
